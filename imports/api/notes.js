f3265549135116c64e8a90d0930de7d3d44c01e0
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-29 21:28:45 +0600

    Add notes.remove method to notes api

--- imports/api/notes.js
+++ imports/api/notes.js
@@ -2,22 +2,42 @@ import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 
 import moment from 'moment';
+import SimpleSchema from 'simpl-schema';
 
 
 export const Notes = new Mongo.Collection('notes');
 
 Meteor.methods({
   'notes.insert'() {
+    // check user authenticity
     if(!this.userId) {
       throw new Meteor.Error('not-authorized');
     }
 
-    // create a note
+    // create note
     return Notes.insert({
       title: '',
       body: '',
       userId: this.userId,
       updatedAt: moment.valueOf()
     });
+  },
+
+  'notes.remove'(_id) {
+    // check user authenticity
+    if(!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    // validate the noteId
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1
+      }
+    }).validate({ _id });
+
+    // remove note
+    return Notes.remove({ _id, userId: this.userId });
   }
 });
