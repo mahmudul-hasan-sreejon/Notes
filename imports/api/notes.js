31ffb933312302f350f2ff2553d993234b7017ff
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-29 20:20:47 +0600

    Add notes.insert method in the notes api

---
+++ imports/api/notes.js
@@ -0,0 +1,23 @@
+import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+
+import moment from 'moment';
+
+
+export const Notes = new Mongo.Collection('notes');
+
+Meteor.methods({
+  'notes.insert'() {
+    if(!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    // create a note
+    return Notes.insert({
+      title: '',
+      body: '',
+      userId: this.userId,
+      updatedAt: moment.valueOf()
+    });
+  }
+});
