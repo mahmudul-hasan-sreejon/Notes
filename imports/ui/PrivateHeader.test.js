8749b527d9ca5b71fd139a35b2cff397b179b63a
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-31 14:40:51 +0600

    Add PrivateHeader testcases for testing

---
+++ imports/ui/PrivateHeader.test.js
@@ -0,0 +1,40 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+
+import expect from 'expect';
+import { mount } from 'enzyme';
+
+import { PrivateHeader } from './PrivateHeader';
+
+
+if(Meteor.isClient) {
+  describe('PrivateHeader', function() {
+    it('should set button text to logout', function() {
+      // add private header
+      const wrapper = mount(<PrivateHeader title="Test title" handleLogout={() => {}} />);
+
+      const buttonText = wrapper.find('button').text();
+
+      expect(buttonText).toBe('Logout');
+    });
+
+    it('should use title prop as h1 text', function() {
+      const title = 'Test title';
+
+      const wrapper = mount(<PrivateHeader title={title} handleLogout={() => {}} />);
+
+      const h1Text = wrapper.find('h1').text();
+
+      expect(h1Text).toBe(title);
+    });
+
+    it('should call handleLogout on click', function() {
+      const spy = expect.createSpy();
+      const wrapper = mount(<PrivateHeader title="Title" handleLogout={spy} />);
+
+      wrapper.find('button').simulate('click');
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+}
