2f92047a02e28d9648e361fb63fe73534e47890c
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-04-10 02:53:26 +0600

    Add server access in notes api

--- server/main.js
+++ server/main.js
@@ -4,6 +4,7 @@ import { WebApp } from 'meteor/webapp';
 import './../imports/startup/simple-schema-config';
 
 import './../imports/api/users';
+import './../imports/api/notes';
 
 
 Meteor.startup(() => {
