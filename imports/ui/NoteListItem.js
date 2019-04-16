3735a1eccec25a00a69309263747df44d04762a3
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-04-17 03:01:09 +0600

    Create NoteListItem component

---
+++ imports/ui/NoteListItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import moment from 'moment';
+
+
+const NoteListItem = props => {
+  return (
+    <div>
+      <h5>{ props.note.title || 'Untitled note' }</h5>
+      <p>{ moment(props.note.updatedAt).format('DD/MM/YYYY') }</p>
+    </div>
+  );
+};
+
+NoteListItem.propTypes = {
+  note: PropTypes.object.isRequired
+};
+
+export default NoteListItem;
