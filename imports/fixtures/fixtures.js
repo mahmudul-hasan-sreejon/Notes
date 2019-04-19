1d6d1883d6c5d0049b8e57d13211a1aaac5ac0c4
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-04-19 16:37:54 +0600

    Add notes fixture

---
+++ imports/fixtures/fixtures.js
@@ -0,0 +1,16 @@
+export const notes = [
+  {
+    _id: 'noteId1',
+    title: 'Test title',
+    body: '',
+    updatedAt: 0,
+    userId: 'userId1'
+  },
+  {
+    _id: 'noteId2',
+    title: '',
+    body: 'Something is here.',
+    updatedAt: 0,
+    userId: 'userId2'
+  }
+];
